5567d6e0f770a9fc6beef68b4b4a3df5fa18c555
Muhammed Rinshad P <m.rinshad210@gmail.com>
2026-07-06 10:32:30 +0530

    Add rule-based roast replies

--- chat/views.py
+++ chat/views.py
@@ -5,10 +5,28 @@ from django.http import HttpResponse
 
 def home(request):
 
-    message = request.GET.get("message")
+    message = request.POST.get("message")
+
+    reply = None
+
+    if message : 
+
+        if "hi" in message.lower():
+            reply = "😂 Bro... First nee urakkathil ninnu ezhunnetto? Pinne hi okke parayam."
+
+        elif "python" in message.lower():
+            reply = "🐍 Python padikkunnundo? Nice... snake alla bro, salary varunna language aanu."
+
+        elif "bmw" in message.lower():
+            reply = "🚗 BMW aano? Aadya cycle odichu balance padikku bro 😂"
+
+        else:
+            reply = f"😂 Bro... '{message}' paranjal njan ippo ithrayee roast cheyyu."
+        
 
     # print(message)
 
     return render(request,"home.html", {
-        "message": message
+        "message": message,
+        "reply": reply
     })
