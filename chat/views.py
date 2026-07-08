1cd3ee0dad7ded5e869230bc1636d668c45c1a87
Muhammed Rinshad P <m.rinshad210@gmail.com>
2026-07-08 21:08:08 +0530

    Learn append and prepare chat history

--- chat/views.py
+++ chat/views.py
@@ -2,31 +2,51 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import HttpResponse
+import random
 
 def home(request):
 
+    messages = []
+
     message = request.POST.get("message")
 
+    # Dictionary (Only for Learning)
+    roasts = {
+
+        "hi": [
+            "😂 Finally ezhunnetto?",
+            "🤣 Coffee kudicho?"
+        ],
+
+        "bmw": [
+            "🚗 Aadya cycle odikku bro.",
+            "😂 BMW undo? Key mathram undo?"
+        ]
+
+    }
+    
     reply = None
 
     if message : 
 
         if "hi" in message.lower():
-            reply = "😂 Bro... First nee urakkathil ninnu ezhunnetto? Pinne hi okke parayam."
+            replies = [
+                "😂 Bro... First nee urakkathil ninnu ezhunnetto? Pinne hi okke parayam.",
+                "🤣 Bro... Coffee kudichittu vaa.",
+                "😎 Finally online vannallo.",
+                ]
+            reply = random.choice(replies)
 
-        elif "python" in message.lower():
-            reply = "🐍 Python padikkunnundo? Nice... snake alla bro, salary varunna language aanu."
-
-        elif "bmw" in message.lower():
-            reply = "🚗 BMW aano? Aadya cycle odichu balance padikku bro 😂"
-
-        else:
-            reply = f"😂 Bro... '{message}' paranjal njan ippo ithrayee roast cheyyu."
+            messages.append({
+                "user": message,
+                "bot": reply,
+            })
         
 
     # print(message)
 
     return render(request,"home.html", {
         "message": message,
-        "reply": reply
+        "reply": reply,
+        "messages": messages,
     })
